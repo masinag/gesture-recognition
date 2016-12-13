fb43be1096f3d2866423d49f81d30a86be1821ef
masinag <gabriele.masina@virgilio.it>
2016-12-13 23:30:14 +0100

    Update the project structure with __init__ and __main__ files

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ for the blurring and the thresholding.
 # Project structure
 
 In the gesture-recognition/ directory you will find the code of the project.
-This includes a __init__.py file which is the one you can run, and a mygesture.py
-file which is a library with useful function for this project.
+This includes a __main__.py file which is the one you can run, and a mygesture.py
+file which is a library with useful function for this project. The __init__.py
+file marks this directory as a python package directory.
 
 In the media/ directory you will find two sub-directories: images/ and videos/,
 which contain some file that you can use as examples.
@@ -32,11 +33,11 @@ Below is described the installation process on Ubuntu 16.10:
 ## Usage
 Below there are some examples of usage:
 
-    $ python main.py --image media/images/one.jpg --blur 40
+    $ python gesture-recognition --image media/images/one.jpg --blur 40
 
-    $ python main.py --image media/images/two.jpg --show -t 150
+    $ python gesture-recognition --image media/images/two.jpg --show -t 150
 
-    $ python main.py --video media/videos/gesture.avi --show
+    $ python gesture-recognition --video media/videos/gesture.avi --show
 
 
 ## Video
---
+++ gesture-recognition/__main__.py
@@ -0,0 +1,44 @@
+"""This program is an example of gesture recognition using Python and OpenCV. It
+allows you to analyze a image, a video or to use your webcam as image source.
+You can also decide the values used to process the image, such as the parameters
+for the blurring and the thresholding.
+
+Examples:
+    $ python gesture-recognition --image media/images/one.jpg --blur 40
+
+    $ python gesture-recognition --image media/images/two.jpg --show -t 150
+
+    $ python gesture-recognition --video media/videos/gesture.avi --show
+
+"""
+if __name__ == '__main__':
+    import mygesture
+    from argparse import ArgumentParser
+
+    ap = ArgumentParser(description = 'This program allows you to analyze a video' +
+                                      ' and recognize hand gestures')
+    ap.add_argument('-b', '--blur', default = 35, type = int,
+                    help = 'Blurring value which represents the grade of blurring' +
+                    'you want')
+    ap.add_argument('-t', '--threshold', default = 127, type = int,
+                    help = 'Thresholding value to recognize the colour of the hand')
+    ap.add_argument('-s', '--show', default = 127, action = 'store_true',
+                    help = 'Show windows the steps of the image processing')
+
+    source = ap.add_mutually_exclusive_group(required = True)
+    source.add_argument('-v', '--video',
+                    help = 'Location of the video you want to analyze')
+    source.add_argument('-w', '--webcam', type = int,
+                    help = 'Use the webcam as video input. The value for the ' +
+                           'default webcam is 0')
+    source.add_argument('-i', '--image',
+                    help = 'Location of the image you want to analyze')
+
+    args = ap.parse_args()
+
+    if args.video:
+        print(mygesture.count_fingers_in_video(args.video, args.show))
+    elif args.webcam>=0:
+        print(mygesture.count_fingers_in_video(args.webcam, args.show))
+    elif args.image:
+        print(mygesture.count_fingers_in_image(args.image, args.show))
